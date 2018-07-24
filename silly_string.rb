c43e3a48c402133c09a4312dfd72669603e90502
izziperron <izziperron@gmail.com>
2018-07-24 14:01:57 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,29 +1,29 @@
 
-# Challenge 1: Fill this string in with your favorite word
+happy
 
-your_favorite_word = ""
+your_favorite_word = "happy"
 
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = happy.upcase
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "MY FAVORITE WORD IS #{your_favorite_word}.upcase"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
 phrase = "IM NOT SHOUTING"
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = "IM NOT SHOUTING".downcase
 
-puts lowercased_phrase
+puts #{lowercased_phrase}
 
 # Challenge 4: Count how many letters are in that big_word
 
-big_word = "supercalifragilisticexpialidocious"
+big_word = "supercalifragilisticexpialidocious big mom".length
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length
 
 puts "There are #{letter_count} in #{big_word}"
 
@@ -31,18 +31,18 @@ puts "There are #{letter_count} in #{big_word}"
 
 bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
 
-new_letter_count = bigger_word.size
+new_letter_count = bigger_word.length
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize
 
 puts proper_sentence
 
 # Challenge 7: String Switcheroo
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub('a','o')
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
